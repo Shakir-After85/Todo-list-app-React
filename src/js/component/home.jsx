1acaeae733f41534cfb9b1978a354af1657b59bd
Shakir-After85 <shakir.tamar@gmail.com>
2022-10-25 03:28:24 +0000

    To Do List w/ React

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	
+	//tasks
+	const[tasks, setTasks] = useState([])
+	
+	//input
+	const[input, setInput] = useState("")
+
+	//handle
+	const handleSubmit = () => {
+
+	}
+
+	//delete task
+	const deleteTask =(id) => {
+		let filteredTasks = tasks.filter();
+	}
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="todo">Daily Goals</h1>
+			<div className="list-card">
+			
+				<form onSubmit={handleSubmit}>
+					<input 
+						type="text" 
+						value={input}
+						onChange={event => setInput(event.target.value)}
+						className="input-box"
+						placeholder="No tasks, add a task"
+					/>
+				</form>
+
+				<div className="list-items">
+					{tasks.map((task) => {
+						<div className="todo" key={task.id}>
+							<p>{task.text}
+								<button 
+								className="button" 
+								onClick={ () => deleteTask(task.id)}> 
+									&#10060;
+								</button>
+							</p>
+						</div>
+					})}
+					<p className="counter">{tasks.length = 1 ? "One task left" : `tasks left : ${tasks.length}` }</p>
+				</div>
+
+			</div>
 		</div>
 	);
 };
